6f40b4ac4e35c4ff864d27f484d9dbb56660fae7
inticoy <inticoy0406@gmail.com>
2023-04-24 21:08:32 +0900

    Feat: get_next_token
    - gets char pointer string
    - returns a token from left

--- src/minishell/token/tokenize.c
+++ src/minishell/token/tokenize.c
@@ -6,9 +6,46 @@
 /*   By: gyoon <gyoon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/24 16:30:40 by gyoon             #+#    #+#             */
-/*   Updated: 2023/04/24 16:40:43 by gyoon            ###   ########.fr       */
+/*   Updated: 2023/04/24 21:06:52 by gyoon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
+
+static int	isquote(int c)
+{
+	if ((char)c == '\'' || (char)c == '\"')
+		return (1);
+	else
+		return (0);
+}
+
+static char	*get_next_token(char *s)
+{
+	char	*token;
+	size_t	len;
+	char	quote;
+
+	len = 0;
+	quote = 0;
+	while (*s == ' ' || *s == '\t')
+		s++;
+	while (s[len])
+	{
+		if (quote && s[len] == quote)
+			break ;
+		else if (!quote && isquote((s[len])))
+			quote = s[len];
+		len++;
+	}
+	if (!len || quote)
+		return (NULL);
+	token = (char *)ft_calloc(len + 1, sizeof(char));
+	ft_strlcpy(token, s, len);
+	return (token);
+}
+
+// 	if (!node || !node.content) needs free?
 
 t_list	*tokenize(char *s)
 {
@@ -23,6 +60,7 @@ t_list	*tokenize(char *s)
 		if (!node || !node.content)
 			break ;
 		ft_lstadd_back(&lst, node);
+		s += ft_strlen((char *)node.content);
 	}
 	return (lst);
 }
