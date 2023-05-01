f29476777cd6eb0879a30515bc586154cd597506
inticoy <inticoy0406@gmail.com>
2023-05-01 13:20:29 +0900

    Delete: operator <>

--- src/minishell/input/token/operator/get_operator_len.c
+++ src/minishell/input/token/operator/get_operator_len.c
@@ -6,7 +6,7 @@
 /*   By: gyoon <gyoon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/25 22:32:34 by gyoon             #+#    #+#             */
-/*   Updated: 2023/04/29 15:57:48 by gyoon            ###   ########.fr       */
+/*   Updated: 2023/05/01 13:19:39 by gyoon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,8 +40,7 @@ static int	get_control_operator_len(char *s)
 
 static int	get_redirection_operator_len(char *s)
 {
-	if (!ft_strncmp(">>", s, 2) || !ft_strncmp("<<", s, 2) || \
-			!ft_strncmp("<>", s, 2))
+	if (!ft_strncmp(">>", s, 2) || !ft_strncmp("<<", s, 2))
 		return (2);
 	else if (!ft_strncmp("<", s, 1) || !ft_strncmp(">", s, 1))
 		return (1);
