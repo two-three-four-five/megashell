8a325518d4bb9cbabea2dd293d6bb2d7b5041a88
gyoon <inticoy0406@gmail.com>
2023-05-18 21:09:10 +0900

    Fix: #25 empty cmd removed

--- src/minishell/input/parse/parse_lst/parse_subshell_with_redirect.c
+++ src/minishell/input/parse/parse_lst/parse_subshell_with_redirect.c
@@ -6,7 +6,7 @@
 /*   By: gyoon <gyoon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/17 16:42:15 by gyoon             #+#    #+#             */
-/*   Updated: 2023/05/18 14:21:15 by gyoon            ###   ########.fr       */
+/*   Updated: 2023/05/18 21:07:09 by gyoon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@
 #include <stdio.h>
 
 static t_list	*find_close_paran_ptr(t_list *lst);
+static t_tree	*parse_remaining_redirect(t_list *lst);
+static t_tree	*parse_redirect(t_list *lst);
 t_tree			*parse_subshell_with_redirect(t_list *lst);
 
 t_tree	*parse_subshell_with_redirect(t_list *lst)
@@ -30,11 +32,9 @@ t_tree	*parse_subshell_with_redirect(t_list *lst)
 	right = close_paren->next;
 	close_paren->next = NULL;
 	head = parse_lst(lst);
-	head->right = parse_lst(right);
+	head->right = parse_remaining_redirect(right);
 	return (head);
 }
-
-// head->right makes empty CMD node
 
 static t_list	*find_close_paran_ptr(t_list *lst)
 {
@@ -54,4 +54,37 @@ static t_list	*find_close_paran_ptr(t_list *lst)
 		lst = lst->next;
 	}
 	return (NULL);
+}
+
+static t_tree	*parse_remaining_redirect(t_list *lst)
+{
+	t_tree	*head;
+	t_list	*prev;
+
+	head = NULL;
+	while (lst)
+	{
+		prev = lst;
+		if (((t_token *)lst->content)->type & REDIRECT)
+		{
+			ft_treeadd_rightend(&head, parse_redirect(lst));
+			lst = lst->next->next;
+			prev->next->next = NULL;
+			ft_lstclear(&prev, del_token);
+		}
+	}
+	return (head);
+}
+
+static t_tree	*parse_redirect(t_list *lst)
+{
+	t_tree	*head;
+
+	head = ft_treenew(\
+				new_token(((t_token *)lst->content)->type, \
+					ft_strdup(((t_token *)lst->content)->token)));
+	head->left = ft_treenew(\
+					new_token(((t_token *)lst->next->content)->type, \
+						ft_strdup(((t_token *)lst->next->content)->token)));
+	return (head);
 }
