fd90324f7f23e7f73ba693ffbb8e72abf53fe104
Jinho Choi <dandyday31@gmail.com>
2023-05-21 15:54:18 +0900

    Fix: Merge Confilct

--- src/minishell/command/execute/execute_cmd.c
+++ src/minishell/command/execute/execute_cmd.c
@@ -6,11 +6,7 @@
 /*   By: jinhchoi <jinhchoi@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/20 13:57:13 by jinhchoi          #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2023/05/21 15:01:04 by jinhchoi         ###   ########.fr       */
-=======
-/*   Updated: 2023/05/21 15:47:55 by jinhchoi         ###   ########.fr       */
->>>>>>> feature/28_exec_node
+/*   Updated: 2023/05/21 15:53:58 by jinhchoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -76,24 +72,11 @@ int	execute_cmd(t_tree *tree, t_dict *env)
 		return (execute_cmd_head(tree, env));
 	else if (!(token->type & HEAD) && is_executable(token->token))
 	{
-<<<<<<< HEAD
-		if (token->type & HEAD)
-			return (execute_cmd_head(tree, env));
-		else
-		{
-			if (redirect_fd(tree) < 0)
-				return (1);
-			argv = get_argv(tree);
-			envp = get_envp(env);
-			execve(token->token, argv, envp);
-		}
-=======
 		if (redirect_fd(tree) < 0)
 			return (1);
 		argv = get_argv(tree);
 		envp = get_envp(env);
 		return (execve(token->token, argv, envp));
->>>>>>> feature/28_exec_node
 	}
 	else
 		raise_exec_error(tree);
