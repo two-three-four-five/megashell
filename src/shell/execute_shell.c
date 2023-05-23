c70e1477b77f4324174aa5f34ab888b7606df244
inticoy <inticoy0406@gmail.com>
2023-05-23 21:12:56 +0900

    Feat: readline colored

--- src/shell/execute_shell.c
+++ src/shell/execute_shell.c
@@ -6,7 +6,7 @@
 /*   By: gyoon <gyoon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/22 16:42:54 by gyoon             #+#    #+#             */
-/*   Updated: 2023/05/23 17:39:32 by gyoon            ###   ########.fr       */
+/*   Updated: 2023/05/23 20:35:44 by gyoon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ void	execute_shell(char **envp)
 	env = get_envp_dict(envp);
 	while (TRUE)
 	{
-		input = readline("dish-0.9$ ");
+		input = readline("\033[0;32mdish-0.9\033[0m$ ");
 		if (!input)
 			raise_exit();
 		ptree = parse_input(input, env);
