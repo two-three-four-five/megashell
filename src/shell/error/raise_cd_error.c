6e1879e1a31015b0f1f04c87062ae1f27f162ad6
inticoy <inticoy0406@gmail.com>
2023-05-26 01:51:49 +0900

    Fix: raise cd error

--- src/shell/error/raise_cd_error.c
+++ src/shell/error/raise_cd_error.c
@@ -6,7 +6,7 @@
 /*   By: gyoon <gyoon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/25 23:52:39 by gyoon             #+#    #+#             */
-/*   Updated: 2023/05/25 23:54:20 by gyoon            ###   ########.fr       */
+/*   Updated: 2023/05/26 01:46:48 by gyoon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 int	raise_cd_error(int errno, char *arg)
 {
 	ft_putstr_fd(MSG_DISH, 2);
+	ft_putstr_fd("cd :", 2);
 	if (errno == NO_SUCH_FILE_OR_DIRECTORY)
 	{
 		ft_putstr_fd(arg, 2);
@@ -32,10 +33,10 @@ int	raise_cd_error(int errno, char *arg)
 		ft_putendl_fd(": Permission denied", 2);
 	}
 	else if (errno == TOO_MANY_ARGUMENTS)
-		ft_putendl_fd("cd: too many arguments", 2);
+		ft_putendl_fd("too many arguments", 2);
 	else if (errno == HOME_NOT_SET)
-		ft_putendl_fd("cd: HOME not set", 2);
+		ft_putendl_fd("HOME not set", 2);
 	else if (errno == OLDPWD_NOT_SET)
-		ft_putendl_fd("cd: OLDPWD not set", 2);
+		ft_putendl_fd("OLDPWD not set", 2);
 	return (1);
 }
