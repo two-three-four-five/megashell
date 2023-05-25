1d4b99fddbd8c0ce8817504e62b967e6595a26c4
inticoy <inticoy0406@gmail.com>
2023-05-26 01:55:35 +0900

    Fix: pipe from back

--- src/shell/input/parse/parse_lst/parse_pipe.c
+++ src/shell/input/parse/parse_lst/parse_pipe.c
@@ -6,7 +6,7 @@
 /*   By: gyoon <gyoon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/17 16:15:28 by gyoon             #+#    #+#             */
-/*   Updated: 2023/05/22 15:51:19 by gyoon            ###   ########.fr       */
+/*   Updated: 2023/05/26 01:53:44 by gyoon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,8 +36,10 @@ t_tree	*parse_pipe(t_list *lst)
 static t_list	*find_pipe_ptr(t_list *lst)
 {
 	int		subshell;
+	t_list	*pipe;
 
 	subshell = 0;
+	pipe = NULL;
 	while (lst)
 	{
 		if (((t_token *)lst->content)->type == OPEN_PAREN)
@@ -45,8 +47,8 @@ static t_list	*find_pipe_ptr(t_list *lst)
 		else if (((t_token *)lst->content)->type == CLOSE_PAREN)
 			subshell--;
 		if (subshell == 0 && ((t_token *)lst->content)->type == PIPE)
-			return (lst);
+			pipe = lst;
 		lst = lst->next;
 	}
-	return (NULL);
+	return (pipe);
 }
