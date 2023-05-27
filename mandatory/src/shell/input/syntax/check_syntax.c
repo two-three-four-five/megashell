22569308ed7e873bd191ede1d2db219ccaddcdc9
inticoy <inticoy0406@gmail.com>
2023-05-27 23:18:24 +0900

    Fix: Syntax error exit status

--- mandatory/src/shell/input/syntax/check_syntax.c
+++ mandatory/src/shell/input/syntax/check_syntax.c
@@ -6,13 +6,13 @@
 /*   By: gyoon <gyoon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/26 21:05:20 by gyoon             #+#    #+#             */
-/*   Updated: 2023/05/26 18:41:24 by gyoon            ###   ########.fr       */
+/*   Updated: 2023/05/27 22:44:20 by gyoon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include "type.h"
-#include "shell/input.h"
+#include "shell.h"
 #include <stdio.h>
 
 t_bool	check_syntax(t_list *lst, t_dict *env)
@@ -21,7 +21,10 @@ t_bool	check_syntax(t_list *lst, t_dict *env)
 	if (!check_undefined_token(lst) || \
 		!check_matching_syntax(lst) || \
 		!check_token_order(lst))
+	{
+		g_exit_status = 2;
 		return (FALSE);
+	}
 	else
 		return (TRUE);
 }
